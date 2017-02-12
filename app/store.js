a6cb268e24b2c0743420f15318475424b37ad972
Nikita Luparev <nikita.luparev@gmail.com>
2017-02-12 22:57:55 +0500

    configured redux dev tools

--- app/store.js
+++ app/store.js
@@ -6,18 +6,19 @@
 * Nick Luparev nikita.luparev@gmail.com
 ------------------------------------------------------------------------------- */
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
 import { hashHistory } from 'react-router';
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const routing = routerMiddleware(hashHistory);
 
 export default function (initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(routing, thunk),
+    composeEnhancers(applyMiddleware(routing, thunk)),
   );
 }
