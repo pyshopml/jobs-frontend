bd3787dd52ba0aea2f432a7f599774c6859bf268
Nikita Luparev <nikita.luparev@gmail.com>
2017-02-12 21:30:57 +0500

    added header component

---
+++ app/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+ 
+import css from './style.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <section className={css.header}>
+
+      </section>
+    );
+  }
+}
+
+export default Header;
--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
- 
+import Header from '../Header';
+
 import css from './style.css';
 
 class Navigation extends Component {
   render() {
     return (
       <section className={css.navigation}>
-
+        <Header />
       </section>
     );
   }
