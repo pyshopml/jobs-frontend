e08996fde7ff6fcab7ac6ad78c482032519f186c
Danil Migachev <dowhileman@gmail.com>
2017-02-15 23:34:56 +0300

    jobs-029: style App container

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -1,18 +1,22 @@
-
 import React, { Component } from 'react'
-import Navigation from '../../components/Navigation';
+import Header from '../../components/Header';
+import AddButton from '../../components/AddPostButton';
 
 import css from './style.css';
 
-interface Props{}
-interface State{}
+interface Props{};
+interface State{};
 
 class App extends Component<Props, State> {
   render() {
     return (
       <section className={css.app}>
-        <Navigation />
-        { this.props.children}
+        <Header />
+        <section className={css.container}>
+          { this.props.children }
+        </section>
+
+        <AddButton />
       </section>
     );
   }
