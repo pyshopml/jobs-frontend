32ae9290124d30eb0147891cfc0a948e95406d77
Danil Migachev <dowhileman@gmail.com>
2017-02-17 17:29:09 +0300

    jobs-033: add Post component

---
+++ app/components/Post/style.scss.d.ts
@@ -0,0 +1,7 @@
+export const post: string;
+export const title: string;
+export const employer: string;
+export const tags: string;
+export const date: string;
+export const tag: string;
+export const info: string
