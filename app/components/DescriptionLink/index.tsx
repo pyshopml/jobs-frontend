b194b8a5bdbef32827e4be6d83c0736d9f820d28
Danil Migachev <dowhileman@gmail.com>
2017-02-22 19:37:33 +0300

    jobs-033:  add DescriptionLink component

---
+++ app/components/DescriptionLink/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface Props{
+  contentState: any;
+  entityKey: any;
+};
+interface State{};
+
+
+class DescriptionLink extends React.Component<Props, State>{
+  render(){
+    const {url} = this.props.contentState.getEntity(this.props.entityKey).getData();
+    return (
+      <a href={url}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+export default DescriptionLink;
