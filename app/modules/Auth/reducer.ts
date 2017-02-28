59fb827a3f844fe968a0dd314677f72d0e42b4af
Nikita Luparev <nikita.luparev@gmail.com>
2017-02-28 13:21:33 +0500

    refactored reducer

--- app/modules/Auth/reducer.ts
+++ app/modules/Auth/reducer.ts
@@ -7,7 +7,6 @@ import {
 
 //import IPost from '../../interfaces/ipost'
 
-
 const initialState = {
   isFetching: false,
   isSuccess: false,
@@ -18,51 +17,24 @@ const initialState = {
 	error: ''
 };
 
-export default(state = initialState, action) =>{
-
+export default (state = initialState, action) => {
   switch (action.type) {
     case AUTH_FETCHING:
-      return {
-	      ...state,
-        isFetching:true,
-	      isSuccess:false,
-	      isError:false,
-	      isSignUp:false,
-      }
+      return Object.assign({}, state, { isFetching: true });
 
     case AUTH_SUCCESS:
 	    const isAuth = action.payload.auth_token ? true : false
-      return {
-	      ...state,
-        isSuccess:true,
-	      isFetching:false,
-	      isError:false,
-	      result:action.payload,
-	      isAuth:isAuth,
-	      error:''
-      }
+      return Object.assign(
+        {},
+        state,
+        { isSuccess: true, isFetching: false, result: action.payload, isAuth: isAuth }
+      );
 
 	  case LOGOUT:
-		  return {
-			  ...state,
-			  isFetching:false,
-			  isSuccess:false,
-			  isError:false,
-			  isAuth:false,
-			  result:{},
-			  error:''
-		  }
+      return Object.assign({}, state, initialState);
 
 	  case SIGN_UP:
-		  return {
-			  isFetching:false,
-			  isSuccess:false,
-			  isError:false,
-			  isAuth:false,
-			  isSignUp:true,
-			  result:action.payload,
-			  error:''
-		  }
+      return Object.assign({}, state, { result: action.payload, isAuth: false, isSignUp:true });
 
     default:
       return state;
