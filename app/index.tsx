f617ea5a44d704d69b029486f3ce4dc68a1e1a60
Nikita Luparev <nikita.luparev@gmail.com>
2017-03-13 10:05:48 +0500

    refactored how store gets configured

--- app/index.tsx
+++ app/index.tsx
@@ -7,18 +7,18 @@
 ------------------------------------------------------------------------------- */
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import createRoutes from './routes';
-import configStore from './store';
+import { configureStore } from './store';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 require('./tap.plugin');
 
 import './index.scss';
 
-const store = configStore({});
-const history = syncHistoryWithStore(browserHistory, store);
+const store = configureStore(hashHistory);
+const history = syncHistoryWithStore(hashHistory, store);
 const routes = createRoutes(store);
 
 const Main = () => (
--- app/store.ts
+++ app/store.ts
@@ -9,10 +9,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
-import { browserHistory } from 'react-router';
 import rootReducer from './reducers';
 import createLogger from 'redux-logger';
 
+/*
 const composeEnhancers  = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const logger = createLogger();
 
@@ -24,4 +24,23 @@ export default function (initialState) {
     initialState,
     composeEnhancers(applyMiddleware(routing, thunk, logger))
   );
+}
+*/
+
+export function configureStore(history, initialState = {}) {
+
+  const composeEnhancers  = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const logger = createLogger();
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(
+      thunk,
+      routerMiddleware(history),
+      logger,
+    )
+  );
+
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  return store;
 }
