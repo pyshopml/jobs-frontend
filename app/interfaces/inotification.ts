891126ca9a55d62c78de5ff70f534e4a1264b838
Danil Migachev <dowhileman@gmail.com>
2017-03-16 16:27:35 +0300

    jobs-066: add notification interface

---
+++ app/interfaces/inotification.ts
@@ -0,0 +1,10 @@
+interface INotification{
+  message: string;
+  type: 'warning' | 'normal';
+  hideDuration?: number;
+  action?: {
+    label: string,
+    onClick: () => void;
+  }
+}
+export default INotification;
