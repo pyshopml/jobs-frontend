75367a3c03c13d23d59ec2a922ed2e0b3de22863
Nikita Luparev <nikita.luparev@gmail.com>
2017-03-19 00:05:46 +0500

    changed how header component is being implemented

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
-import Logo from '../Logo';
+import { Button } from 'elemental';
+import { Link } from 'react-router';
+import Logo from './components/Logo';
 
-import Auth from '../../containers/Auth';
+// import Auth from '../../containers/Auth';
 
 import css from './style.scss';
 
 interface Props{};
 interface State{};
 
+const style = {
+  margin: 12,
+};
+
 class Header extends Component<Props, State> {
+	LoginBar() {
+    return (
+      <section className={css.authControls}>
+        <Button type="link-primary" size="xs">
+          <Link to="/login">Вход</Link>
+        </Button>
+        <Button type="success" size="xs">
+          <Link to="/signup">Регистрация</Link>
+        </Button>
+      </section>
+    );
+  }
+
   render() {
     return (
-	    <section className={css.header}>
-		    <section className={css.container}>
+	    <header className={css.header}>
+		    <div className={css.container}>
 			    <Logo />
-			    <Auth />
-		    </section>
-	    </section>
+					{ this.LoginBar() }
+		    </div>
+	    </header>
     )
   }
 }
