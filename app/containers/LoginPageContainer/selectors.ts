8e0ddb79f4484149bf1af75facaefb4d0b30375e
Nikita Luparev <nikita.luparev@gmail.com>
2017-03-18 21:13:36 +0500

    fixed issue with in progress state is not showing

--- app/containers/LoginPageContainer/selectors.ts
+++ app/containers/LoginPageContainer/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectSubstate = state => state.global.login;
+const selectSubstate = state => state.global.loginPage;
 
 export default createSelector(
   selectSubstate,
