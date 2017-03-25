d5eef554768d4ade7678515a2ecf3a6e9f921aca
Danil Migachev <dowhileman@gmail.com>
2017-03-25 21:24:23 +0300

    jobs-086; remove tooltip classname

--- app/components/TooltipWrapper/index.tsx
+++ app/components/TooltipWrapper/index.tsx
@@ -49,7 +49,6 @@ class TooltipWrapper extends React.Component<Props, State>{
       >
         <Tooltip label={this.props.label}
                  show={this.state.showTooltip}
-                 className={ css.tooltip }
                  verticalPosition={this.props.verticalPosition}
                  horizontalPosition={this.props.horizontalPosition}
         />
