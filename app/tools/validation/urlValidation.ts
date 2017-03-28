6c002330fcaf49be173eae6fee768a83688af8ee
Danil Migachev <dowhileman@gmail.com>
2017-03-28 12:40:51 +0300

    remove console.log && update regex url pattern

--- app/tools/validation/urlValidation.ts
+++ app/tools/validation/urlValidation.ts
@@ -1,9 +1,8 @@
 export default function urlValidation(url: string){
   if(!url)
     return false;
-  console.log(url)
   const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+.[a-z]{2,})'+ // domain name
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+\\.[a-z]{2,})'+ // domain name
     '(\\/[-a-z\\d%_.~+]*)*'+ // path
     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
