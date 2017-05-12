c32f58147caeb301d71726f305fbcb039fbb3436
Danil Migachev <dowhileman@gmail.com>
2017-05-12 11:38:38 +0300

    jobs-091: Extend vacancy model

--- app/models/Post.class.ts
+++ app/models/Post.class.ts
@@ -4,6 +4,19 @@ class PostClass{
   readonly user: string;
   readonly title: string;
   readonly description: string;
+  readonly salary_min: number;
+  readonly salary_max: number;
+  readonly location: {
+    readonly city: string;
+    readonly country: string;
+  };
+  readonly keywords: string[];
+  readonly busyness: number;
+  readonly remote_work: boolean;
+  readonly category: {
+    title: string;
+    id: number;
+  }
   private _created_on?: Date;
   private _modified_on?: Date;
   constructor(postData){
@@ -12,6 +25,13 @@ class PostClass{
     this.user = postData.user;
     this.title = postData.title;
     this.description = postData.description;
+    this.salary_min = postData.salary_min;
+    this.salary_max = postData.salary_max;
+    this.location = postData.location;
+    this.keywords = postData.keywords;
+    this.busyness = postData.busyness;
+    this.remote_work = postData.remote_work;
+    this.category = postData.category;
     this._created_on = new Date(postData.created_on);
     this._modified_on = new Date(postData.modified_on);
   }
